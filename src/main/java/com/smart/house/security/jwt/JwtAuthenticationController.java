2dc44ac294658b87db1abb85f9cd4ccd42868383
Mr-Yassine <mr.yassine.bilal@gmail.com>
2022-05-09 11:45:32 +0200

    first commit

---
+++ src/main/java/com/smart/house/security/jwt/JwtAuthenticationController.java
@@ -0,0 +1,65 @@
+package com.smart.house.security.jwt;
+
+import com.smart.house.entity.JwtRequest;
+import com.smart.house.entity.JwtResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+@RestController
+@CrossOrigin
+public class JwtAuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserDetailsService jwtInMemoryUserDetailsService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> generateAuthenticationToken(@RequestBody JwtRequest authenticationRequest)
+            throws Exception {
+
+        Boolean isAuth = authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        if(isAuth){
+            final UserDetails userDetails = jwtInMemoryUserDetailsService
+                    .loadUserByUsername(authenticationRequest.getUsername());
+
+            final String token = jwtTokenUtil.generateToken(userDetails);
+
+            return ResponseEntity.ok(new JwtResponse(token));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new JwtResponse("can't get the token: bad credentials"));
+        }
+
+    }
+
+    private Boolean authenticate(String username, String password) throws Exception {
+        Objects.requireNonNull(username);
+        Objects.requireNonNull(password);
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            return true;
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            System.out.println("authenticate username: " + username);
+            //throw new Exception("INVALID_CREDENTIALS", e);
+            System.out.println("exception"+ e.getMessage());
+            return false;
+        }
+    }
+}
